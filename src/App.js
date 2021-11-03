4e6d92075224d6076caf44d4604e441e2c63236f
Jan Rolenc <jrolenc@centrum.cz>
2021-11-03 11:35:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [joke, setJoke] = useState("");
+
+  async function getJoke() {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const data = await response.json();
+    setJoke(data.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="mainPage">
+      <div className="heroDiv">
+        <div className="heroText">
+          <h1>I am Chuck Norris</h1>
+          <h3>And I'm a HERO!</h3>
+        </div>
+      </div>
+      <div className="jokeDiv">
+        <p><button onClick={getJoke}>Click for new joke</button>{joke}</p>
+      </div>
     </div>
+
   );
 }
-
 export default App;
